f0fac2ddb8c0f29d14d232e00b115acbf6b3b14e
Gavin <gavin.drumm2@mail.dcu.ie>
2017-03-22 22:43:44 +0000

    add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'seller' => "listings#seller"
+
+
   root 'listings#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
