30bd0bc435e1eaa0a8a5726250ffa3a7a6e36e12
Gavin <gavin.drumm2@mail.dcu.ie>
2017-03-22 21:21:48 +0000

    Associate listings w/ Users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,4 +6,6 @@ class Listing < ActiveRecord::Base
 	  validates :price, numericality: { greater_than: 0 }
 	  validates :image, presence: true
 
+	  belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322172231) do
+ActiveRecord::Schema.define(version: 20170322210804) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170322172231) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
