eb18e58a7864d366d9d33185f7908040410cf513
Gavin <gavin.drumm2@mail.dcu.ie>
2017-03-23 15:11:35 +0000

    add address to listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -75,7 +75,7 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:name, :description, :price, :image)
+      params.require(:listing).permit(:name, :description, :address, :price, :image)
     end
     def check_user
       if current_user != @listing.user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322210804) do
+ActiveRecord::Schema.define(version: 20170323145417) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170322210804) do
     t.datetime "updated_at",  null: false
     t.string   "image"
     t.integer  "user_id"
+    t.string   "address"
   end
 
   create_table "users", force: :cascade do |t|
