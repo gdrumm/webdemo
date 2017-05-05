57e392951bdae0aa45f409bdf3a5da9e41d16db8
Gavin <gavin.drumm2@mail.dcu.ie>
2017-05-05 11:40:00 +0100

    Copyright

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   resources :listings do
     collection do
       get 'search'
     end
 		resources :listings, :address
-
 end		
   get 'pages/about'
 
   get 'pages/contact'
 
   get 'seller' => "listings#seller"
-
 
   root 'listings#index'
 
